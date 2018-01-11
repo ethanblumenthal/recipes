385e3018d02b616e1be57ef36037cc375c5e7298
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-10 16:56:28 -0800

    added Navbar component and styling

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Recipe from './Recipe';
+import Navbar from './Navbar';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Recipe
           title="pasta"
           ingredients={['flour', 'water']}
