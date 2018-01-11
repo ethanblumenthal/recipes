7b86cdf86a8681c5720e9c52c80c17d3090ac580
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-11 10:51:23 -0800

    added return statement for RecipeInput

--- src/RecipeInput.js
+++ src/RecipeInput.js
@@ -14,7 +14,13 @@ class RecipeInput extends Component {
             ingredients: [''],
             img: ''
         };
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render() {
         const {title, instructions, ingredients, img} = this.state;
         const {oncClose} = this.props;
@@ -36,6 +42,75 @@ class RecipeInput extends Component {
                 </label>
             </div>
         ));
+    }
+    return (
+        <div className="recipe-form-container">
+          <form className='recipe-form' onSubmit={() => {}}>
+            <button
+              type="button"
+              className="close-button"
+              onClick={onClose}
+            >
+              X
+            </button>
+            <div className='recipe-form-line'>
+              <label htmlFor='recipe-title-input'>Title</label>
+              <input
+                id='recipe-title-input'
+                key='title'
+                name='title'
+                type='text'
+                value={title}
+                size={42}
+                autoComplete="off"
+                onChange={this.handleChange}/>
+            </div>
+            <label
+              htmlFor='recipe-instructions-input'
+              style={{marginTop: '5px'}}
+            >
+              Instructions
+            </label>
+            <textarea
+              key='instructions'
+              id='recipe-instructions-input'
+              type='Instructions'
+              name='instructions'
+              rows='8'
+              cols='50'
+              autoComplete='off'
+              value={instructions}
+              onChange={this.handleChange}/>
+            {inputs}
+            <button
+              type="button"
+              onClick={this.handleNewIngredient}
+              className="buttons"
+            >
+              +
+            </button>
+            <div className='recipe-form-line'>
+              <label htmlFor='recipe-img-input'>Image Url</label>
+              <input
+                id='recipe-img-input'
+                type='text'
+                placeholder=''
+                name='img'
+                value={img}
+                size={36}
+                autoComplete='off'
+                onChange={this.handleChange} />
+            </div>
+            <button
+              type="submit"
+              className="buttons"
+              style={{alignSelf: 'flex-end', marginRight: 0}}
+            >
+              SAVE
+            </button>
+          </form>
+        </div>
+      )
     }
 }
 
